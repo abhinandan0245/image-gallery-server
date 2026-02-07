cc1210f4d8fe701a6ee1b8358ff8a22fbfde28c2
ABHINANDAN GUPTA <abhig0245@gmail.com>
2026-02-07 14:48:26 +0530

    domain

--- src/app.js
+++ src/app.js
@@ -1,3 +1,28 @@
+// import express from "express";
+// import cors from "cors";
+// import adminRoutes from "./routes/adminRoutes.js";
+// import imageRoutes from "./routes/imageRoutes.js";
+// import authRoutes from "./routes/authRoutes.js";
+
+// const app = express();
+
+// //  CORS CONFIG
+// app.use(
+//   cors({
+//     origin: ["http://localhost:5175", "http://localhost:5176" ,"https://image-gallery-client-aksk09i7y-abhinandan-guptas-projects.vercel.app" ,"https://image-gallery-abhi.netlify.app"],
+//     credentials: true,
+//   })
+// );
+
+// app.use(express.json());
+
+// app.use("/api/admin", adminRoutes);
+// app.use("/api/images", imageRoutes);
+// app.use("/api/auth", authRoutes);
+
+// export default app;
+
+
 import express from "express";
 import cors from "cors";
 import adminRoutes from "./routes/adminRoutes.js";
@@ -6,16 +31,32 @@ import authRoutes from "./routes/authRoutes.js";
 
 const app = express();
 
-//  CORS CONFIG
+const allowedOrigins = [
+  "http://localhost:5175",
+  "http://localhost:5176",
+  "https://image-gallery-client-aksk09i7y-abhinandan-guptas-projects.vercel.app",
+  "https://image-gallery-abhi.netlify.app", // NO trailing slash
+];
+
 app.use(
   cors({
-    origin: ["http://localhost:5175", "http://localhost:5176" ,"https://image-gallery-client-aksk09i7y-abhinandan-guptas-projects.vercel.app" ,"https://image-gallery-abhi.netlify.app/"],
+    origin: function (origin, callback) {
+      // allow requests with no origin (like Postman)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg = `The CORS policy for this site does not allow access from the specified Origin.`;
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
     credentials: true,
   })
 );
 
+// parse JSON
 app.use(express.json());
 
+// routes
 app.use("/api/admin", adminRoutes);
 app.use("/api/images", imageRoutes);
 app.use("/api/auth", authRoutes);
