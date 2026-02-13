ecaceea775ab24a36e487eb941367b6d86d0a5d9
ABHINANDAN GUPTA <abhig0245@gmail.com>
2026-02-13 12:36:17 +0530

    firbase

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,34 +1,17 @@
-import admin from "firebase-admin";
-import fs from "fs";
-import path from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const serviceAccountPath = path.join(__dirname, "../../serviceAccountKey.json");
-
-if (!admin.apps.length) {
-  const serviceAccount = JSON.parse(
-    fs.readFileSync(serviceAccountPath, "utf-8"),
-  );
-
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-  });
-}
-
-export default admin;
-
-
 // import admin from "firebase-admin";
-    
-// if (!process.env.FIREBASE_CREDENTIALS) {
-//   throw new Error("FIREBASE_CREDENTIALS is missing in environment variables");
-// }
+// import fs from "fs";
+// import path from "path";
+// import { fileURLToPath } from "url";
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+
+// const serviceAccountPath = path.join(__dirname, "../../serviceAccountKey.json");
 
 // if (!admin.apps.length) {
-//   const serviceAccount = JSON.parse(process.env.FIREBASE_CREDENTIALS);
+//   const serviceAccount = JSON.parse(
+//     fs.readFileSync(serviceAccountPath, "utf-8"),
+//   );
 
 //   admin.initializeApp({
 //     credential: admin.credential.cert(serviceAccount),
@@ -36,3 +19,20 @@ export default admin;
 // }
 
 // export default admin;
+
+
+import admin from "firebase-admin";
+    
+if (!process.env.FIREBASE_CREDENTIALS) {
+  throw new Error("FIREBASE_CREDENTIALS is missing in environment variables");
+}
+
+if (!admin.apps.length) {
+  const serviceAccount = JSON.parse(process.env.FIREBASE_CREDENTIALS);
+
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+}
+
+export default admin;
